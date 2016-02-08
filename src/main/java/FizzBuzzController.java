020774c773da4b989caa05de905a0a6a76ae7f23
andrevalgrande <andre.valgrande@gmail.com>
2016-02-08 16:32:48 +0200

    Changed 'usage' message.

--- src/main/java/FizzBuzzController.java
+++ src/main/java/FizzBuzzController.java
@@ -6,7 +6,7 @@ import org.restexpress.Response;
 
 public class FizzBuzzController
 {
-  private static final String USAGE = "Please call GET and set query-string parameter 'numbers' (such as numbers=0,2,20,300).";
+  private static final String USAGE = "Please call GET and set either the request header or query-string 'numbers' parameter (such as numbers=0,2,20,300).";
 
   public String create(Request request, Response response) throws Exception
   {
